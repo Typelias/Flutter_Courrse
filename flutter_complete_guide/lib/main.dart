5f484dae7a755dbd74d4e18c5f5984b03461a4d7
typelias <98ille@gmail.com>
2021-01-09 01:28:46 +0100

    Scaffold widget compleete

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
+/*void main() {
   runApp(MyApp());
-}
+}*/
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('Default Text'),
+      ),
     );
   }
 }
