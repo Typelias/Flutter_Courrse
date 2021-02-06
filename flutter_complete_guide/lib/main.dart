7e4f7ebc32bc21af3a4fba3263702cf37fa8fbbc
typelias <98ille@gmail.com>
2021-02-07 00:58:19 +0100

    We have buttons and Statefull Widgets

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 
-/*void main() {
-  runApp(MyApp());
-}*/
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+
+    print('Answer Chosen!');
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your favourite color?', 'What\'s your favourite animal?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('Default Text'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Hello'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {
+                //...
+                print("Greger")
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
